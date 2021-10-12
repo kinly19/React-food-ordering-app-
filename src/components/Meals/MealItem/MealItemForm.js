9510e398c345e46be5c0ff1da3417a8e8fb4a02b
unknown <kinchoily@hotmail.com>
2021-10-12 15:25:05 +0100

    Created MealItemForm component and styling

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,23 @@
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id:'amount',
+          type:'Number', //inputs type
+          min: '1', //inputs amount has to be more than 1 
+          max: '5', //not more than 5
+          step: '1', //amount to add on click
+          defaultValue:'1' //start value
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
