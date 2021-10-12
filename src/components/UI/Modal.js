dd15debc2b4c54cce0740e3b436ee947df6b6e84
unknown <kinchoily@hotmail.com>
2021-10-12 17:09:00 +0100

    Modal component and styling

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,40 @@
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
+//backdrop compoent for layout
+const BackDrop = () => {
+  return <div className={classes.backdrop}></div>;
+};
+
+//overlay component, this is where we will use props.chidren to pass content inbetween the tags
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+//react portal needs to know what and where to portal 
+//ReactDOM.createPortal(child, container)
+// The first argument (child) is any renderable React child, such as an element, string, or fragment. 
+// The second argument (container) is a DOM element.
+
+//const helper
+const portalElement = document.getElementById('overlays');
+
+//the actual component being exported, which will make use of both Backdrop and ModalOverlay components above with React Portal.
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<BackDrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
