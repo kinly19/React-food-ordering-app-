548f3bd558d4010982694ce9cc192184721d9bf7
unknown <kinchoily@hotmail.com>
2021-10-13 18:01:29 +0100

    HeaderCartButton.js providing CartContext data to component, to allow the button to show carts quantity

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,16 @@
+import { useContext } from 'react'; //useContext to use data within CartProvider
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext); //just like useState, whenever context changes the HeaderCartButton value will also update
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
@@ -9,7 +18,7 @@ const HeaderCartButton = (props) => {
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}> {/* output total amount of items inside of cart */}
-        3
+        {numberOfCartItems}
       </span>
     </button>
   );
