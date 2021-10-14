7b116d694374b7ab3bb0a5b9143d360700384642
unknown <kinchoily@hotmail.com>
2021-10-14 16:57:15 +0100

    CartProvider updated logic and handlers, for updating CartContext

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,21 +1,53 @@
 //this component will manage the cart context data, and provide the context (data) to all components that want or need access to it.
-//this allow us to point to the data we need directly instead of passing data around via prop chain
+//this allows us to point to the data we need directly instead of passing data around via prop chain
+
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
+//default state which we will use inside of useReducer()
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
 
-  const addItemToCartHandler = (item) => {
-
+// --> points too
+//reducer function state --> defaultCartState is the last state snapshot managed by the reducer function. action is dispatched by us. 
+const cartReducer = (state, action) => { //'state'--> cartState --> useReducer --> defaultCartState
+  if(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item); //whatever the state was plus action.items
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount; //eg action.item.price(5)* action.item.amount(2) + state.totalAmount (5) = 15
+     //return a new state snapshot
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
   }
+  //return default if above does not meet
+  return defaultCartState
+};
+
+const CartProvider = (props) => {
+  //using Reducer function created above
+  //first argument (useReducer()) we point at our reducer function, 2nd argument we point at our defaultCartState.
+  //useReducer will return an array with exactly two elements, so we can use array destructuring to pull these elements out of array.
+  //The first element is our state snapshot, 2nd element is a function that will allow us to dispatch an action to the useReducer 
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  //when this function is called, we take the item and add it to cart, but check if this 'item' we add is already inside the cart
+  //we update the existing item otherwise add the new item
+  const addItemToCartHandler = (item) => { //totally up to user what the action is, but typically its an object with some property that allows us to identify that action inside of our reducer function
+    dispatchCartAction({type:'ADD', item: item}); // --> useReducer --> cartRudcer function
+  };
 
   const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type:'REMOVE', id: id});
+  };
 
-  }
-
-  const cartContext = { //this is where we will make changes to our context
-    items: [],
-    totalAmount: 0,
-    addItem: addItemToCartHandler, //point to our functions above (like props.addItem={addItemToCartHandler})
+  //use the cartState to construct cartContext object 
+  const cartContext = { 
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler, //point to our functions above
     removeItem: removeItemFromCartHandler
   }
 
