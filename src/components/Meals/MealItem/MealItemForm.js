2f342f3a850709b2ab87a0aa8297600b90dc82a3
unknown <kinchoily@hotmail.com>
2021-10-14 17:02:37 +0100

    MealItemForm.js update to form to warn users of entering a valid amount and handling of form on submit

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,34 @@
+import { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = (props) => {
+  
+  const amountInputRef = useRef();
+  const [amountValid, setAmountIsValid] = useState(true);
+
+  const sumbitHandler = (e) => {
+    
+    e.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      //this will stop the form from doing anything if the enteredAmount is 0 or greater than 5
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={sumbitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id:`amount_${props.id}`,
@@ -16,6 +40,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
