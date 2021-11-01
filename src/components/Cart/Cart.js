381f586bb0ceb933ab8a435f5825d819fbe212a6
unknown <kinchoily@hotmail.com>
2021-11-01 16:33:32 +0000

    Cart.js - created state and a handler for conditional rendering of Checkout component, with the use of a helper const for keeping jsx snippet clean

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,17 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
+
+//======================== Notes ========================
+//props.onHideCart --> app.js
+//=======================================================
 
 const Cart = (props) => {
+
+  const [isCheckout, setIsCheckout] = useState(false);
 
   const cartCtx = useContext(CartContext);
   const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}` //template literal
@@ -16,6 +23,10 @@ const Cart = (props) => {
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount:1}); //this will run addItemToCartHandler function inside of CartProvider
+  };
+
+  const orderHandler = () => {
+    setIsCheckout(true);
   };
 
   //helper const 
@@ -34,17 +45,28 @@ const Cart = (props) => {
     </ul>
   );
 
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onHideCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
   return (
-    <Modal onClick={props.onHideCart}>{/* Overlay component used as a wrapper */}
+    <Modal onClick={props.onHideCart}> {/* Overlay component used as a wrapper */}
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+      {isCheckout && <Checkout onCancel={props.onHideCart}/>}
+      {!isCheckout && modalActions}
     </Modal>
   );
 };
