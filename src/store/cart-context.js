fc8195dc1fc00e12b5dc2b2414ccc5a7b25318f4
unknown <kinchoily@hotmail.com>
2021-11-01 18:44:43 +0000

    cart-context.js - add clearCart function

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -3,13 +3,9 @@ import React from 'react';
 const CartContext = React.createContext({
   items: [], //
   totalAmount: 0, //
-  addItem: (item) => { //functions to update items context
-
-  },
-  removeItem: (id) => { //function to remove items from context
-
-  }
-
+  addItem: (item) => {}, //functions to update items context
+  removeItem: (id) => {}, //function to remove items from context
+  clearCart: () => {}
 });
 
 export default CartContext;
